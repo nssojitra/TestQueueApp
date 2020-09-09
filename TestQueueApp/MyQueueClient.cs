dad3456cbc7a756dfded68e6fe42e5075372ed17
Nayankumar Sojitra <SojitraNayankumar@JohnDeere.com>
2020-09-09 08:30:56 +0530

    Add project files.

---
+++ TestQueueApp/MyQueueClient.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.ServiceBus.Messaging;
+
+namespace TestQueueApp
+{
+    public class QueueConnection
+    { 
+        private QueueClient client;
+        public QueueConnection()
+        {
+            client = QueueClient.CreateFromConnectionString("Endpoint=sb://testnayan.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=/kr73MZ6aqFSSeH7naEYJf2bJhDbdXHgCJBYwhk1+ik=", "testquueue");
+        }
+        
+        public void SendMessage(BrokeredMessage message)
+        {
+            client.SendAsync(message).Wait();
+        }
+
+        public void DeadLetter(BrokeredMessage message)
+        {
+            message.DeadLetter("", "");
+        }
+
+        public void Abandon(BrokeredMessage message)
+        {
+            message.Abandon();
+        }
+
+        public BrokeredMessage Receive()
+        {
+            
+            return client.ReceiveAsync().Result;
+        }
+
+        public int CountMessages()
+        {
+            var ret = 0;
+            BrokeredMessage m;
+            while((m=client.PeekAsync().Result)!=null)
+            {
+                ret++;
+            }
+            return ret;
+        }
+
+        public void Delete(BrokeredMessage message)
+        {
+            message.Complete();
+        }
+    }
+}
---
+++ TestQueueApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestQueueApp
+{
+    class Program
+    {
+        static BrokeredMessage GetBrokeredMeaagse(string s)
+        {
+            return new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(s)));
+        }
+        static QueueConnection conn = new QueueConnection();
+
+        static void Main(string[] args)
+        {
+            //for (var i = 0; i < 50; i++)
+            //{
+            //    var message = GetBrokeredMeaagse($"This is a test message {i+1}");
+            //    message.Properties.Add("MessageId", Guid.NewGuid().ToString());
+            //    message.Properties.Add("MessageType", "WOSegmentCreatedInDbs");
+            //    conn.SendMessage(message);
+            //}
+
+            Console.WriteLine($"No Of Messgaes: {conn.CountMessages()}");
+
+            Console.ReadLine();
+
+        }
+
+        static void Receive()
+        {
+            BrokeredMessage message;
+            var icount = 1;
+            while ((message = conn.Receive()) != null)
+            {
+                Console.WriteLine($"Message {icount++}:");
+                message.Properties.ToList().ForEach(x =>
+                {
+                    Console.WriteLine($"{x.Key}: {x.Value}");
+                });
+
+                string body = "";
+
+                using (var stream = new StreamReader(message.GetBody<Stream>()))
+                {
+                    body = stream.ReadToEnd();
+                }
+                Console.WriteLine($"====================================================");
+                conn.Delete(message);
+            }
+
+        }
+    }
+}
